6e0e304b3fe557b9f37ad6606f5efedd09945805
Abner.Lima <abnerlima.1997@gmail.com>
2022-01-23 07:31:37 -0300

    Criando receita.model

---
+++ src/receitas/receita.model.ts
@@ -0,0 +1,29 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+@Table
+export class Receita extends Model {
+    @Column({
+        primaryKey: true,
+        type: DataType.INTEGER,
+        autoIncrement: true
+    })
+    id: number;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    descricao: string;
+
+    @Column({
+        type: DataType.FLOAT,
+        allowNull: false
+    })
+    valor: number;
+
+    @Column({
+        type: DataType.DATEONLY,
+        allowNull: false
+    })
+    data: Date;
+}
